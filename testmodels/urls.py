c3e77d7974d67a273e582842a1e06011fa27c06d
adri-2 <www.adriensani@gmail.com>
2026-02-02 13:45:48 +0100

    fix ci psql #6

--- testmodels/urls.py
+++ testmodels/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('app.urls'))
+    path('api/v1/',include('app.urls')),
   
 ]
 if settings.DEBUG:
